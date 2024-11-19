d3b520a1aef97812a6657227542f7b079c332a5f
oleg_medovikov <oleg.medovikov@mail.ru>
2024-11-19 12:36:02 +0300

    update loginCheck js

--- static/js/loginCheck.js
+++ static/js/loginCheck.js
@@ -1,14 +1,19 @@
 // Проверяем наличие токена в localStorage
-if (!localStorage.getItem('authToken')) {
+const authToken = localStorage.getItem('authToken');
+
+if (!authToken) {
   // Если токена нет, перенаправляем на страницу входа
   window.location.href = '/login.html';
 } else {
   // Отправляем запрос к API для проверки токена
-  fetch(`/check_token`, {
-    method: 'GET',
+  fetch('/api/check_token', {
+    method: 'POST',
     headers: {
-      'Authorization': localStorage.getItem('authToken')
-    }
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ 
+      token: authToken
+    })
   })
   .then(response => {
     if (!response.ok) {
@@ -20,31 +25,11 @@ if (!localStorage.getItem('authToken')) {
     if (!data.token_valid) {
       // Если токен не валиден, перенаправляем на страницу входа
       window.location.href = '/login.html';
-    } else { // Если токен валидный - мы вытаскиваем пользователя
-      fetch(`/user_get`, { 
-        method: 'GET',
-        headers: {
-          'Authorization': localStorage.getItem('authToken')
-        }
-      })
-      .then(response => {
-        if (!response.ok) {
-          throw new Error('Network response was not ok');
-        }
-        return response.json(); // Преобразуем тело ответа в JSON
-      })
-      .then(data => {  
-        localStorage.setItem('fio', data.fio); 
-        localStorage.setItem('groups', data.groups);
-        document.getElementById('UserFIO').innerHTML = localStorage.getItem('fio');
-        document.getElementById('UserGroups').innerHTML = localStorage.getItem('groups');
-      })
-      .catch(error => {
-        console.error('There has been a problem with your fetch operation:', error);
-      });
     }
   })
   .catch(error => {
     console.error('There was a problem with the fetch operation:', error);
+    // В случае ошибки также перенаправляем на страницу входа
+    window.location.href = '/login.html';
   });
 }
